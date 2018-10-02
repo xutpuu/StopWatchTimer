5bf4940c32b81eda3301ce5b1185db6736e983fb
Egor Shatrov <xutpuu@gmail.com>
2018-10-03 00:09:16 +0300

    first commit

---
+++ Timer/Main.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace Timer
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.tbTimer = new System.Windows.Forms.TextBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(15, 38);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(188, 38);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // tbTimer
+            // 
+            this.tbTimer.Location = new System.Drawing.Point(90, 12);
+            this.tbTimer.Name = "tbTimer";
+            this.tbTimer.ReadOnly = true;
+            this.tbTimer.Size = new System.Drawing.Size(100, 20);
+            this.tbTimer.TabIndex = 3;
+            // 
+            // timer
+            // 
+            this.timer.Interval = 10;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(281, 74);
+            this.Controls.Add(this.tbTimer);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Main";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.TextBox tbTimer;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ Timer/Main.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Timer
+{
+    public partial class Main : Form
+    {
+
+        private DateTime start = DateTime.MinValue;
+        private TimeSpan oldTime = TimeSpan.Zero;
+        const double _n = 3;
+
+        public Main()
+        {
+            InitializeComponent();
+            timer = new System.Windows.Forms.Timer();
+            timer.Interval = 10;
+            timer.Tick += new EventHandler(timer_Tick);
+        }
+
+        private void btnStart_Click(object sender, System.EventArgs e)
+        {
+            timer.Start();
+            start = DateTime.Now.AddSeconds(_n);
+        }
+
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            oldTime = (start - DateTime.Now);
+            tbTimer.Text = oldTime.ToString("c");
+            if (oldTime.Milliseconds == 0)
+                timer.Stop();
+        }
+    }
+}
+
